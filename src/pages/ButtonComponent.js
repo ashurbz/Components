23e0d17966843679803819b92b390658abc6d9b8
Ashish <rbzashu1@gmail.com>
2023-05-31 17:55:35 +0530

    configured tailwind css and created components

---
+++ src/pages/ButtonComponent.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Button from "../components/Button";
+
+const ButtonComponent = () => {
+  return (
+    <div>
+      <Button>Click Me</Button>
+    </div>
+  );
+};
+
+export default ButtonComponent;
