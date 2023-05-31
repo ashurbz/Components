57bb30521e6a3f91bc063473851670b4528aae7e
Ashish <rbzashu1@gmail.com>
2023-05-31 18:16:32 +0530

    added all things in button, button component compl

--- src/components/Button.js
+++ src/components/Button.js
@@ -10,17 +10,27 @@ const Button = ({
   danger,
   outline,
   rounded,
+  ...rest
 }) => {
-  const classes = classNames("px-3  py-1.5 border", {
+  const classes = classNames("px-3  py-1.5 border", rest.className, {
     "bg-blue-500 border-blue-500 text-white ": primary,
     "bg-gray-500 border-gray-500 text-white ": secondary,
     "bg-green-500 border-green-500 text-white ": success,
     "bg-yellow-500 border-yellow-500 text-white ": warning,
     "bg-red-500 border-red-500 text-white ": danger,
+    "rounded-full": rounded,
+    "bg-white": outline,
+    "text-blue-500": outline && primary,
+    "text-gray-500": outline && secondary,
+    "text-green-500": outline && success,
+    "text-yellow-500": outline && warning,
+    "text-red-500": outline && danger,
   });
   return (
     <div>
-      <button className={classes}>{children}</button>
+      <button {...rest} className={classes}>
+        {children}
+      </button>
     </div>
   );
 };
