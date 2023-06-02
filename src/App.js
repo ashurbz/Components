a052571402d6602f765618162842241c8f3db6f7
Ashish <rbzashu1@gmail.com>
2023-06-02 10:33:21 +0530

    added title and components for dropdown

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import AccordianPage from "./pages/AccordianPage";
 import ButtonPage from "./pages/ButtonPage";
+import DropDownPage from "./pages/DropdownPage";
 
 function App() {
   return (
     <div className="App">
       <ButtonPage />
       <AccordianPage />
+      <DropDownPage />
     </div>
   );
 }
---
+++ src/pages/DropdownPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Dropdown from "../components/Dropdown";
+
+const DropdownPage = () => {
+  return (
+    <div>
+      <Dropdown />
+    </div>
+  );
+};
+
+export default DropdownPage;
