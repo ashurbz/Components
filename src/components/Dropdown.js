41d7ccf919526111c9cc62916c4ce42c07301638
Ashish <rbzashu1@gmail.com>
2023-06-02 11:16:36 +0530

    created dropdown and fixed styling

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,7 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Dropdown = () => {
-  return <div>Dropdown</div>;
+const Dropdown = ({ options, selected, onSelect }) => {
+  const [isShown, setIsShown] = useState(false);
+
+  const handleClick = () => {
+    setIsShown(!isShown);
+  };
+
+  const handleOptionClick = (option) => {
+    setIsShown(false);
+    onSelect(option.option);
+  };
+
+  return (
+    <>
+      <div>
+        <h3 className="p-2 m-4 bg-gray-400 w-20" onClick={handleClick}>
+          {selected ? selected : "Select..."}
+        </h3>
+        {options.map((option) => {
+          return (
+            <div
+              className="m-4"
+              key={option.value}
+              onClick={() => {
+                handleOptionClick(option);
+              }}
+            >
+              {isShown && option.option}{" "}
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
 };
 
 export default Dropdown;
--- src/pages/DropdownPage.js
+++ src/pages/DropdownPage.js
@@ -1,10 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import Dropdown from "../components/Dropdown";
 
+const listItems = [
+  {
+    option: "green",
+    value: "green",
+  },
+  {
+    option: "blue",
+    value: "blue",
+  },
+  {
+    option: "yellow",
+    value: "yellow",
+  },
+];
+
 const DropdownPage = () => {
+  const [selected, setSelected] = useState(null);
+
+  const onSelect = (option) => {
+    setSelected(option);
+  };
+
   return (
     <div>
-      <Dropdown />
+      <Dropdown selected={selected} onSelect={onSelect} options={listItems} />
     </div>
   );
 };
