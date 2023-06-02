5881c8874d9c242951b1ef009be5e4fdff6b70ae
Ashish <rbzashu1@gmail.com>
2023-06-02 10:15:21 +0530

    mock created for Accordian

--- src/components/Accordian.js
+++ src/components/Accordian.js
@@ -1,17 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Accordian = ({ items }) => {
+  const [expandedIndex, setExpandedIndex] = useState(0);
+
   return (
-    <div>
-      {items.map((item) => {
-        return (
-          <div key={item.id}>
-            <h3>{item.label}</h3>
-            <span>{item.content}</span>
-          </div>
-        );
-      })}
-    </div>
+    <>
+      <div>
+        {items.map((item, index) => {
+          const expanded = index === expandedIndex;
+          return (
+            <>
+              <div className="flex" key={item.id}>
+                <h3
+                  onClick={() => {
+                    setExpandedIndex(index);
+                  }}
+                >
+                  {item.label}
+                </h3>
+                <span>{expanded ? "Down" : "Left"}</span>
+              </div>
+              <div>
+                <span>{expanded && item.content}</span>
+              </div>
+            </>
+          );
+        })}
+      </div>
+    </>
   );
 };
 
