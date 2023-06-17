eeacbae68440c16376bb69cb412d6bcd80bf184c
Ashish <rbzashu1@gmail.com>
2023-06-17 11:04:39 +0530

    completed with modal functionality

--- src/Route.js
+++ src/Route.js
@@ -1,9 +1,8 @@
-import React from "react";
 import useNavigationHook from "./hooks/useNavigationHook";
 
 const Route = ({ path, children }) => {
   const { currentPath } = useNavigationHook();
-  console.log(path, children);
+
   if (path === currentPath) {
     return children;
   }
--- src/pages/ModalPage.js
+++ src/pages/ModalPage.js
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import Modal from "../components/Modal";
+import Button from "../components/Button";
 
 const ModalPage = () => {
+  const [show, setShow] = useState(false);
   return (
     <div>
-      <Modal />
+      <Button
+        primary
+        onClick={() => {
+          setShow(!show);
+        }}
+      >
+        Open Modal
+      </Button>
+      {show && <Modal isShow={setShow} />}
     </div>
   );
 };
